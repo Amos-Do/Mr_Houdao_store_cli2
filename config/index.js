4a6eec69014e4dcb9f2c71d853c36c2d97970ebe
Amos-Do <0424028@nkust.edu.tw>
2020-07-02 09:11:41 +0800

    修改 bulid 路徑

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/Mr_Houdao_store_cli2/',
 
     /**
      * Source Maps
